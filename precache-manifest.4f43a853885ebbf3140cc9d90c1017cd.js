2a270d89e7b785f68ffeed195f9a45944ec0599c
jakobthefrog <100538052+jakobthefrog@users.noreply.github.com>
2022-02-27 17:22:30 -0700

    Updates

---
+++ precache-manifest.4f43a853885ebbf3140cc9d90c1017cd.js
@@ -0,0 +1,258 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d439d4074822b53aa8830c0d58c93b3",
+    "url": "/UXPortfolio/index.html"
+  },
+  {
+    "revision": "17ed078fee9e0c06ca52",
+    "url": "/UXPortfolio/static/css/4.0d92772c.chunk.css"
+  },
+  {
+    "revision": "a538b73a784ac33af7e3",
+    "url": "/UXPortfolio/static/css/main.f09be8ab.chunk.css"
+  },
+  {
+    "revision": "f9a11c66daa50d005c3b",
+    "url": "/UXPortfolio/static/js/0.a979a534.chunk.js"
+  },
+  {
+    "revision": "7f2d66d6e482edb3b7aa",
+    "url": "/UXPortfolio/static/js/10.fd9fb42d.chunk.js"
+  },
+  {
+    "revision": "158d191c7d0292882998",
+    "url": "/UXPortfolio/static/js/11.9aad23b9.chunk.js"
+  },
+  {
+    "revision": "e19cff098fc6634bc17f",
+    "url": "/UXPortfolio/static/js/12.ab300f5d.chunk.js"
+  },
+  {
+    "revision": "91c126a9d3a6788c1a99",
+    "url": "/UXPortfolio/static/js/13.ac12c8af.chunk.js"
+  },
+  {
+    "revision": "9865bd1b779bca24e509",
+    "url": "/UXPortfolio/static/js/14.5f1ddd9c.chunk.js"
+  },
+  {
+    "revision": "3be2a4f0c61ee0a61b71",
+    "url": "/UXPortfolio/static/js/15.98d10ee1.chunk.js"
+  },
+  {
+    "revision": "6e487c5f4c80615a30e1",
+    "url": "/UXPortfolio/static/js/16.96306161.chunk.js"
+  },
+  {
+    "revision": "67457d8be38708997cf6",
+    "url": "/UXPortfolio/static/js/17.deb46f9f.chunk.js"
+  },
+  {
+    "revision": "2b9d80ddbfe80752c0e8",
+    "url": "/UXPortfolio/static/js/18.3ee89fef.chunk.js"
+  },
+  {
+    "revision": "cea3ef6839de2747634e",
+    "url": "/UXPortfolio/static/js/19.f8ab36d0.chunk.js"
+  },
+  {
+    "revision": "35a40d065c271aac6f0f",
+    "url": "/UXPortfolio/static/js/20.9e49a5c9.chunk.js"
+  },
+  {
+    "revision": "95f9d641101968ae776a",
+    "url": "/UXPortfolio/static/js/21.3c409740.chunk.js"
+  },
+  {
+    "revision": "7cad486aaf9409174908",
+    "url": "/UXPortfolio/static/js/22.6e88f60b.chunk.js"
+  },
+  {
+    "revision": "08032b6dc9a095c4db52",
+    "url": "/UXPortfolio/static/js/23.2301d3ac.chunk.js"
+  },
+  {
+    "revision": "c74d7634cc376f6d8156",
+    "url": "/UXPortfolio/static/js/24.bf784fd9.chunk.js"
+  },
+  {
+    "revision": "c3a701dd9259afa16fff",
+    "url": "/UXPortfolio/static/js/25.a69924b4.chunk.js"
+  },
+  {
+    "revision": "87448a9ad37fadd7b530",
+    "url": "/UXPortfolio/static/js/26.66ea8982.chunk.js"
+  },
+  {
+    "revision": "db5e43b6fcfc977cf990",
+    "url": "/UXPortfolio/static/js/27.0bcdf7f8.chunk.js"
+  },
+  {
+    "revision": "5fa012f6b0b5c84c994b",
+    "url": "/UXPortfolio/static/js/28.11b62d75.chunk.js"
+  },
+  {
+    "revision": "c40af6f82941d259f699",
+    "url": "/UXPortfolio/static/js/29.a48813a1.chunk.js"
+  },
+  {
+    "revision": "e8e7b5f0253bbc6e3ef2",
+    "url": "/UXPortfolio/static/js/30.45ec85c5.chunk.js"
+  },
+  {
+    "revision": "fb0774e0c91840d8f980",
+    "url": "/UXPortfolio/static/js/31.3d265819.chunk.js"
+  },
+  {
+    "revision": "501154006429aa95a215",
+    "url": "/UXPortfolio/static/js/32.e217b21e.chunk.js"
+  },
+  {
+    "revision": "54bcce60c9244cb4385c",
+    "url": "/UXPortfolio/static/js/33.d2affd1e.chunk.js"
+  },
+  {
+    "revision": "55f6582a802c27fa2034",
+    "url": "/UXPortfolio/static/js/34.b01d72c0.chunk.js"
+  },
+  {
+    "revision": "2532fbe917bd728edcca",
+    "url": "/UXPortfolio/static/js/35.0cc59242.chunk.js"
+  },
+  {
+    "revision": "2b38911bfc3ea68fbe57",
+    "url": "/UXPortfolio/static/js/36.36c07acc.chunk.js"
+  },
+  {
+    "revision": "cd4fda6f9a8e103ecee1",
+    "url": "/UXPortfolio/static/js/37.ae0f2ce3.chunk.js"
+  },
+  {
+    "revision": "883de89368f12da90154",
+    "url": "/UXPortfolio/static/js/38.e0939d32.chunk.js"
+  },
+  {
+    "revision": "6585c23a7e1fe53387ca",
+    "url": "/UXPortfolio/static/js/39.44be9b40.chunk.js"
+  },
+  {
+    "revision": "17ed078fee9e0c06ca52",
+    "url": "/UXPortfolio/static/js/4.f376de68.chunk.js"
+  },
+  {
+    "revision": "5b571a43117d7de158db9e221eaa0225",
+    "url": "/UXPortfolio/static/js/4.f376de68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85a267382a7369df038d",
+    "url": "/UXPortfolio/static/js/40.8c6416bd.chunk.js"
+  },
+  {
+    "revision": "f94b15e1e2977901510d",
+    "url": "/UXPortfolio/static/js/41.4c708343.chunk.js"
+  },
+  {
+    "revision": "9f67e0c41359e6f7446e",
+    "url": "/UXPortfolio/static/js/42.eb6a554d.chunk.js"
+  },
+  {
+    "revision": "a7ca87c83ba25864ff1e",
+    "url": "/UXPortfolio/static/js/43.6d7c9783.chunk.js"
+  },
+  {
+    "revision": "514e84894c98f57bb281",
+    "url": "/UXPortfolio/static/js/44.b774886a.chunk.js"
+  },
+  {
+    "revision": "726fedd04f4869347b19",
+    "url": "/UXPortfolio/static/js/45.230f5abe.chunk.js"
+  },
+  {
+    "revision": "48945cd87858d87815f2",
+    "url": "/UXPortfolio/static/js/46.80579dbf.chunk.js"
+  },
+  {
+    "revision": "90541f2035eeb65a16ae",
+    "url": "/UXPortfolio/static/js/47.2452dbdc.chunk.js"
+  },
+  {
+    "revision": "387cd7862667f5c2e8dc",
+    "url": "/UXPortfolio/static/js/48.b0f4be60.chunk.js"
+  },
+  {
+    "revision": "edad547c27117c5ed975",
+    "url": "/UXPortfolio/static/js/49.31fb6c60.chunk.js"
+  },
+  {
+    "revision": "f9dc0920f9327ae8bfbf",
+    "url": "/UXPortfolio/static/js/5.9bc31039.chunk.js"
+  },
+  {
+    "revision": "d83b29cf653dd9644ea9",
+    "url": "/UXPortfolio/static/js/50.f5b9cd78.chunk.js"
+  },
+  {
+    "revision": "6b627da8ded6cffeb859",
+    "url": "/UXPortfolio/static/js/51.8931e41e.chunk.js"
+  },
+  {
+    "revision": "e59abb6975671fb9cdcd",
+    "url": "/UXPortfolio/static/js/52.007472b6.chunk.js"
+  },
+  {
+    "revision": "08cedd436e3ae763f873",
+    "url": "/UXPortfolio/static/js/53.1c07cccf.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/UXPortfolio/static/js/53.1c07cccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10ccb0028924e2400336",
+    "url": "/UXPortfolio/static/js/54.76c0d69a.chunk.js"
+  },
+  {
+    "revision": "fce7f58cdbbb64b83e48",
+    "url": "/UXPortfolio/static/js/55.0d30982e.chunk.js"
+  },
+  {
+    "revision": "d1a1a718a036c328d563",
+    "url": "/UXPortfolio/static/js/56.9e5f2952.chunk.js"
+  },
+  {
+    "revision": "d1c50a2ee23b2818c6bb",
+    "url": "/UXPortfolio/static/js/57.62158d2f.chunk.js"
+  },
+  {
+    "revision": "1a04de3d3fab0d4e8431",
+    "url": "/UXPortfolio/static/js/6.3db46746.chunk.js"
+  },
+  {
+    "revision": "200f75363974b47ff121",
+    "url": "/UXPortfolio/static/js/7.0a450741.chunk.js"
+  },
+  {
+    "revision": "f5aee72e2f8443d7c7ed",
+    "url": "/UXPortfolio/static/js/8.d02b5954.chunk.js"
+  },
+  {
+    "revision": "b6b306ed15ed44a1b929",
+    "url": "/UXPortfolio/static/js/9.2f923165.chunk.js"
+  },
+  {
+    "revision": "a538b73a784ac33af7e3",
+    "url": "/UXPortfolio/static/js/main.c5fad436.chunk.js"
+  },
+  {
+    "revision": "638e46b46c61b1f6194a",
+    "url": "/UXPortfolio/static/js/polyfills-dom.2dc1a73d.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/UXPortfolio/static/js/polyfills-dom.2dc1a73d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "163b95220f9d630cca4c",
+    "url": "/UXPortfolio/static/js/runtime-main.468ccae6.js"
+  }
+]);
